58f27da9dbaf412e91cfd6876fb303bc43371f44
Noah Stapp <noah.stapp@mongodb.com>
2025-08-15 12:25:54 -0400

    Fix perf test path

--- .evergreen/run-perf-tests.sh
+++ .evergreen/run-perf-tests.sh
@@ -10,11 +10,4 @@ export OUTPUT_FILE="results.json"
 python -m pip install -U pip
 pip install -e .
 
-# Install django and test dependencies
-git clone --branch mongodb-5.2.x https://github.com/mongodb-forks/django django_repo
-pushd django_repo/tests/
-pip install -e ..
-pip install -r requirements/py3.txt
-popd
-
-python run_perf_test.py
+python .evergreen/run_perf_test.py
